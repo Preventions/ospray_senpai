3fbaad33e66c3510f60d949b17574c225dc2a23f
Will Usher <will@sci.utah.edu>
2019-12-16 09:45:27 -0700

    Update tfn widget, add image write

--- util/util.cpp
+++ util/util.cpp
@@ -1,19 +1,22 @@
 #include "util.h"
 
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
 std::string pretty_print_count(const double count) {
-	const double giga = 1000000000;
-	const double mega = 1000000;
-	const double kilo = 1000;
-	if (count > giga) {
-		return std::to_string(count / giga) + " G";
-	} else if (count > mega) {
-		return std::to_string(count / mega) + " M";
-	} else if (count > kilo) {
-		return std::to_string(count / kilo) + " K";
-	}
-	return std::to_string(count);
+    const double giga = 1000000000;
+    const double mega = 1000000;
+    const double kilo = 1000;
+    if (count > giga) {
+        return std::to_string(count / giga) + " G";
+    } else if (count > mega) {
+        return std::to_string(count / mega) + " M";
+    } else if (count > kilo) {
+        return std::to_string(count / kilo) + " K";
+    }
+    return std::to_string(count);
 }
 
 uint64_t align_to(uint64_t val, uint64_t align) {
-	return ((val + align - 1) / align) * align;
+    return ((val + align - 1) / align) * align;
 }
